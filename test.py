713ddfd3eb9df07be75aa9ce2db49688064adc2f
TXH2020 <87963860+TXH2020@users.noreply.github.com>
2023-02-04 17:56:17 +0530

    Update test.py

--- test.py
+++ test.py
@@ -3,4 +3,15 @@ r=requests.post('http://localhost:5000',{"hello":"three"})
 print(r.text)
 
 """import subprocess
-x=subprocess.Popen(['cmd'],text=True)"""
+x=subprocess.Popen(['cmd'],text=True)
+import json
+import sys
+import subprocess
+command=json.loads(sys.argv[1]).get('Name')
+call = subprocess.Popen(["bash"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+output, errors = call.communicate(input=command)
+call.wait()
+if(len(output)!=0):
+	print(output)
+if(len(errors)!=0):
+	print(errors)"""
